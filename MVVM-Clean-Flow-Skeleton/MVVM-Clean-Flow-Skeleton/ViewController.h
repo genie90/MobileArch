68060b1b763f66fbc7fdb4af95e9126ad9978ca4
Truong Quoc Viet (Genie) <viet.tr90@gmail.com>
2020-07-13 09:35:18 +0700

    initial

---
+++ MVVM-Clean-Flow-Skeleton/MVVM-Clean-Flow-Skeleton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MVVM-Clean-Flow-Skeleton
+//
+//  Created by Genie Truong on 13/7/20.
+//  Copyright © 2020 Genie Truong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MVVM-Clean-Flow-Skeleton/MVVM-Clean-Flow-Skeleton/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  MVVM-Clean-Flow-Skeleton
+//
+//  Created by Genie Truong on 13/7/20.
+//  Copyright © 2020 Genie Truong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end

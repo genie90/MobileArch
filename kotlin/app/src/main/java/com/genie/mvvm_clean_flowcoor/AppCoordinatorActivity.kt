babb82cf311995a50281b13f58a5877c91b434e5
Truong Quoc Viet (Genie) <viet.tr90@gmail.com>
2020-07-30 12:53:52 +0700

    implement coor

--- kotlin/app/src/main/java/com/genie/mvvm_clean_flowcoor/AppCoordinatorActivity.kt
+++ kotlin/app/src/main/java/com/genie/mvvm_clean_flowcoor/AppCoordinatorActivity.kt
@@ -1,12 +1,29 @@
 package com.genie.mvvm_clean_flowcoor
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.genie.mvvm_clean_flowcoor.auth.AuthCoordinatorActivity
 
 class AppCoordinatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_app_coordinator)
+        launchApp()
     }
+
+    private fun launchApp() {
+        var login = false
+        if (login) {
+            // Show something else
+        } else {
+            showAuthFlow()
+        }
+    }
+
+    private fun showAuthFlow() {
+        startActivity(Intent(this, AuthCoordinatorActivity::class.java))
+    }
+
 }
--- kotlin/app/src/main/java/com/genie/mvvm_clean_flowcoor/auth/AuthCoordinatorActivity.kt
+++ kotlin/app/src/main/java/com/genie/mvvm_clean_flowcoor/auth/AuthCoordinatorActivity.kt
@@ -2,12 +2,29 @@ package com.genie.mvvm_clean_flowcoor.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
 import com.genie.mvvm_clean_flowcoor.R
+import com.genie.mvvm_clean_flowcoor.auth.login.LoginFragment
 
 class AuthCoordinatorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth_coordinator)
+        showLoginScreen()
     }
+
+    private fun showLoginScreen() {
+        showAnAuthScreen(LoginFragment.newInstance())
+    }
+
+    private fun showAnAuthScreen(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.authContainer, fragment)
+            .commit()
+    }
+
+    interface 
 }
